5874d951808e0a5166a1f36e6fd2171e820a12c5
bowmandaApp <bowmanda@appstate.edu>
2023-04-05 18:33:50 -0400

    second update- electric money

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -17,7 +17,14 @@ export class HomeComponent {
   transistor = 0;
   flywheel = 0;
   flywheelUpgrade = 1;
+  transformer = 0;
+  transformerUpgrade = 1;
+  pamphlets = 0;
+  paper = 0;
+  ink = 0;
+  printerUpgrade = 1;
   money = 0.00;
+  printerProfit = 0.00;
   copperCoils = 0;
   tubeVal: number[] = [];
   vacuum: string[] = [];
@@ -30,6 +37,13 @@ export class HomeComponent {
   lockFlywheel = false;
   startFlywheel = false;
   showMachines = false;
+  showPrinterInfo = false;
+  ownPrinter = false;
+  startPrinting = false;
+  ownTransformer = false;
+  startTransforming = false;
+  showTransformer = false;
+
 
   crank_images = [
     'assets/crank.png',
@@ -59,27 +73,27 @@ export class HomeComponent {
     this.loopEnabled = !this.loopEnabled;
     this.startFlywheel = true;
     if (this.loopEnabled) {
-      this.startLoop(); 
+      this.startLoop();
     } else {
       this.stopLoop();
     }
   }
-  
+
   startLoop() {
-      this.currentFlyIndex = 1;
-   setInterval(() => {
-    this.chargeFlywheel();
+    this.currentFlyIndex = 1;
+    setInterval(() => {
+      this.chargeFlywheel();
     }, 1000);
   }
   stopLoop() {
     this.startFlywheel = false;
     this.currentFlyIndex = 0;
     setInterval(() => {
-      
+
 
     }, 0);
   }
-  
+
   get currentFlyImage() {
     return this.flywheel_images[this.currentFlyIndex];
   }
@@ -98,7 +112,7 @@ export class HomeComponent {
 
     const money = localStorage.getItem('money');
     if (money !== null) {
-    this.money = parseFloat(money);
+      this.money = parseFloat(money);
     }
 
     const tubes = localStorage.getItem('tubes');
@@ -123,17 +137,17 @@ export class HomeComponent {
       }
     }
 
-    
+
     const coils = localStorage.getItem('copperCoils');
-if (coils !== null) {
-  this.copperCoils = parseInt(coils, 10);
-  if (this.copperCoils > 0) {
-    this.showMachines = true;
-  }
-  for (let i = 0; i < this.copperCoils; i++) {
-    this.coils.push('assets/coils.png');
-  }
-}
+    if (coils !== null) {
+      this.copperCoils = parseInt(coils, 10);
+      if (this.copperCoils > 0) {
+        this.showMachines = true;
+      }
+      for (let i = 0; i < this.copperCoils; i++) {
+        this.coils.push('assets/coils.png');
+      }
+    }
 
 
     const ownFlywheel = localStorage.getItem('ownFlywheel');
@@ -147,30 +161,74 @@ if (coils !== null) {
     }
     const flywheelUpgrade = localStorage.getItem('flywheelUpgrade');
     if (flywheelUpgrade !== null) {
-    this.flywheelUpgrade = parseFloat(flywheelUpgrade);
+      this.flywheelUpgrade = parseFloat(flywheelUpgrade);
     }
 
+    const ownPrinter = localStorage.getItem('ownPrinter');
+    if (ownPrinter !== null) {
+      this.ownPrinter = JSON.parse(ownPrinter);
+    }
+
+    const pamphlets = localStorage.getItem('pamphlets');
+    if (pamphlets !== null) {
+      this.pamphlets = JSON.parse(pamphlets);
+    }
+    const printerUpgrade = localStorage.getItem('printerUpgrade');
+    if (printerUpgrade !== null) {
+      this.printerUpgrade = parseFloat(printerUpgrade);
+    }
+
+    const paper = localStorage.getItem('paper');
+    if (paper !== null) {
+      this.paper = JSON.parse(paper);
+    }
+
+    const ink = localStorage.getItem('ink');
+    if (ink !== null) {
+      this.ink = JSON.parse(ink);
+    }
+
+    const ownTransformer = localStorage.getItem('ownTransformer');
+    if (ownTransformer !== null) {
+      this.ownTransformer = JSON.parse(ownTransformer);
+    }
+
+    const transformerUpgrade = localStorage.getItem('transformerUpgrade');
+    if (transformerUpgrade !== null) {
+      this.transformerUpgrade = parseFloat(transformerUpgrade);
+    }
+
+    const transformer = localStorage.getItem('transformer');
+    if (transformer !== null) {
+      this.ink = JSON.parse(transformer);
+    }
+
+    const printerProfit = localStorage.getItem('printerProfit');
+    if (printerProfit !== null) {
+      this.printerProfit = JSON.parse(printerProfit);
+    }
 
     setInterval(() => {
 
       for (let i = 0; i < this.tubes; i++) {
-
         this.energy++;
       }
 
       for (let i = 0; i < this.transistor; i++) {
-
-        this.energy += 10;
+        this.energy += 12;
       }
-      //this.energy += this.tubes;
+
+      if (this.ownPrinter == true && this.pamphlets > 0) {
+        this.sellPamphlets();
+      }
       localStorage.setItem('energy', this.energy.toString());
     }, 1000);
   }
-  
+
   incrementClicks() {
     this.clicks++;
     this.energyCount();
-    
+
     if (this.clicks === 4) {
       this.incrementScore();
 
@@ -187,6 +245,12 @@ if (coils !== null) {
     localStorage.removeItem('money');
     this.tubes = 0;
     localStorage.removeItem('tubes');
+    this.paper = 0;
+    localStorage.removeItem('paper');
+    this.ink = 0;
+    localStorage.removeItem('ink');
+    this.pamphlets = 0;
+    localStorage.removeItem('pamphlets');
     this.vacuum = [];
     this.transistor = 0;
     localStorage.removeItem('transistor');
@@ -194,13 +258,25 @@ if (coils !== null) {
     this.copperCoils = 0;
     localStorage.removeItem('copperCoils');
     this.coils = [];
+    this.flywheel = 0;
     this.flywheelUpgrade = 1;
     localStorage.removeItem('flywheelUpgrade');
     this.showTubeInfo = false;
     this.showMachines = false;
     this.showTransInfo = false;
     this.showFlywheelInfo = false;
+    this.showTransformer = false;
     this.ownFlywheel = false;
+    this.showPrinterInfo = false;
+    this.printerEnabled = false;
+    this.ownPrinter = false;
+    this.printerUpgrade = 1;
+    localStorage.removeItem('printerUpgrade');
+    localStorage.removeItem('printerProfit');
+    this.ownTransformer = false;
+    this.transformerEnabled= false;
+    this.transformerUpgrade = 1;
+    localStorage.removeItem('transformerUpgrade');
   }
 
   incrementScore() {
@@ -214,15 +290,21 @@ if (coils !== null) {
     if (this.transistor > 0) {
       this.transistors;
     }
-    if (this.score >= 50){
+    if (this.score >= 50) {
       this.showTransInfo = true;
     }
-    if (this.score >= 100){
+    if (this.score >= 100) {
       this.showFlywheelInfo = true;
     }
-
-    if (this.score >= 200 && this.flywheel >= 250){
+    if (this.flywheel >= 500) {
+      this.showTransformer = true;
+    }
+    if (this.score >= 200 && this.flywheel >= 250) {
       this.showMachines = true;
+    }
+
+    if (this.score >= 700 && this.flywheel >= 1000) {
+      this.showPrinterInfo = true;
     }
     localStorage.setItem('score', this.score.toString());
   }
@@ -241,14 +323,14 @@ if (coils !== null) {
 
   copperWireCoils() {
     this.copperCoils += this.energy;
-    this.flywheel -= 5*this.copperCoils;
+    this.flywheel -= 5 * this.copperCoils;
     this.coils.push('assets/coils.png');
     localStorage.setItem('copperCoils', this.copperCoils.toString());
   }
 
   energyCount() {
-    if(this.copperCoils > 0){
-    this.energy+= this.copperCoils * 2;
+    if (this.copperCoils > 0) {
+      this.energy += this.copperCoils * 2;
     } else {
       this.energy++;
     }
@@ -269,7 +351,7 @@ if (coils !== null) {
     }
   }
   removeTube(index: number) {
-        this.vacuum.splice(index, 1);
+    this.vacuum.splice(index, 1);
   }
 
   addTrans() {
@@ -284,9 +366,9 @@ if (coils !== null) {
       }
 
     }
-          localStorage.setItem('tubes', this.tubes.toString());
-      localStorage.setItem('transistor', this.transistor.toString());
-      localStorage.setItem('energy', this.energy.toString());
+    localStorage.setItem('tubes', this.tubes.toString());
+    localStorage.setItem('transistor', this.transistor.toString());
+    localStorage.setItem('energy', this.energy.toString());
   }
 
   addCoils() {
@@ -301,35 +383,174 @@ if (coils !== null) {
     }
   }
 
-  sellEnergy(){
-     this.money += this.energy * 0.05;
-     this.energy = 0;
-     localStorage.setItem('money', this.money.toString());
+  sellEnergy() {
+    this.money += this.energy * 0.05;
+    this.energy = 0;
+    localStorage.setItem('money', this.money.toString());
   }
 
-  buyFlywheel(){
+  buyFlywheel() {
     this.money -= 5;
     this.ownFlywheel = true;
     localStorage.setItem('ownFlywheel', 'true');
   }
 
-  upgradeFlywheel(){
-    if (this.flywheelUpgrade < 1){
+  upgradeFlywheel() {
+    if (this.flywheelUpgrade < 1) {
       this.flywheelUpgrade = 1;
     }
-    if((this.energy > 100 * this.flywheelUpgrade) && (this.energy >= 100 * this.flywheelUpgrade)){
-    this.energy -= 100 * this.flywheelUpgrade;
-    this.money -= 100 * this.flywheelUpgrade;
-    this.flywheelUpgrade++;
-    localStorage.setItem('flywheelUpgrade', this.flywheelUpgrade.toString());
+    if ((this.energy > 100 * this.flywheelUpgrade) && (this.money >= 10 * this.flywheelUpgrade)) {
+      this.energy -= 100 * this.flywheelUpgrade;
+      this.money -= 10 * this.flywheelUpgrade;
+      this.flywheelUpgrade++;
+      localStorage.setItem('flywheelUpgrade', this.flywheelUpgrade.toString());
     }
   }
-  chargeFlywheel(){
-    if(this.energy > 5 * this.flywheelUpgrade &&  this.loopEnabled == true){
-    this.score++;
-    this.energy -= 5 * this.flywheelUpgrade;
-    this.flywheel += 5 * this.flywheelUpgrade;
-    localStorage.setItem('flywheel', this.flywheel.toString());
+  chargeFlywheel() {
+    if (this.energy > 5 * this.flywheelUpgrade && this.loopEnabled == true) {
+      this.score++;
+      this.energy -= 5 * this.flywheelUpgrade;
+      this.flywheel += 5 * this.flywheelUpgrade;
+      localStorage.setItem('flywheel', this.flywheel.toString());
+    }
   }
+
+  buyPrinter() {
+    if(this.money > 500){
+    this.money -= 500;
+    this.ownPrinter = true;
+    localStorage.setItem('ownPrinter', 'true');
+    }
+  }
+
+  upgradePrinter() {
+    if (this.printerUpgrade < 1) {
+      this.printerUpgrade = 1;
+    }
+    if ((this.money > 500 * this.printerUpgrade) && (this.energy >= 1000 * this.printerUpgrade)) {
+      this.energy -= 1000 * this.printerUpgrade;
+      this.money -= 500 * this.printerUpgrade;
+      this.printerUpgrade++;
+      localStorage.setItem('PrinterUpgrade', this.printerUpgrade.toString());
+    }
+  }
+  runPrinter() {
+    if (this.flywheel > 100 * this.printerUpgrade && this.printerEnabled == true && this.paper > 9 && this.ink > 1) {
+      this.score++;
+      this.paper -= 10 * this.printerUpgrade;
+      this.ink -= 2 * this.printerUpgrade;
+      this.pamphlets += 10 * this.printerUpgrade;
+      localStorage.setItem('pamphlets', this.pamphlets.toString());
+      localStorage.setItem('ink', this.ink.toString());
+      localStorage.setItem('paper', this.paper.toString());
+    }
+  }
+
+
+  printerEnabled: boolean = false;
+
+  togglePrinter() {
+    this.printerEnabled = !this.printerEnabled;
+    this.startPrinting = true;
+    if (this.printerEnabled) {
+      this.startPrinter();
+    } else {
+      this.stopPrinter();
+    }
+  }
+
+
+  startPrinter() {
+    setInterval(() => {
+      this.runPrinter();
+      this.flywheel -= 500;
+    }, 5000);
+
+  }
+  stopPrinter() {
+    this.startPrinting = false;
+    setInterval(() => {
+    }, 0);
+
+  }
+
+  buyPaper() {
+    if (this.money > 19.99) {
+      this.paper += 100;
+      this.money -= 19.99;
+      localStorage.setItem('paper', this.paper.toString());
+    }
+  }
+  buyInk() {
+    if (this.money > 29.99) {
+      this.money -= 29.99;
+      this.ink += 20;
+      localStorage.setItem('ink', this.ink.toString());
+    }
+  }
+
+  sellPamphlets() {
+    this.pamphlets -= 1;
+    this.money += 0.50;
+    this.printerProfit += 0.50; 
+    localStorage.setItem('printerProfit', this.printerProfit.toString());
+    localStorage.setItem('pamphlets', this.pamphlets.toString());
+  }
+
+  transformerEnabled: boolean = false;
+
+  toggleTransformer() {
+    this.transformerEnabled = !this.transformerEnabled;
+    this.startTransforming = true;
+    if (this.transformerEnabled) {
+      this.startTransformer();
+    } else {
+      this.stopTransformer();
+    }
+  }
+
+  startTransformer() {
+    setInterval(() => {
+      this.runTransformer();
+    }, 1000);
+
+  }
+  stopTransformer() {
+    this.startTransforming= false;
+    setInterval(() => {
+    }, 0);
+
+  }
+
+  buyTransformer() {
+    if (this.money > 30){
+    this.money -= 30;
+    this.ownTransformer = true;
+    localStorage.setItem('ownTransformer', 'true');
+    }
+  }
+
+  upgradeTransformer() {
+    if (this.transformerUpgrade < 1) {
+      this.transformerUpgrade = 1;
+    }
+    if ((this.money > 25 * this.transformerUpgrade) && (this.energy >= 100 * this.transformerUpgrade)) {
+      this.energy -= 100 * this.transformerUpgrade;
+      this.money -= 25 * this.transformerUpgrade;
+      this.transformerUpgrade++;
+      localStorage.setItem('PrinterUpgrade', this.transformerUpgrade.toString());
+    }
+  }
+  runTransformer() {
+    if (this.flywheel > 2 * this.transformerUpgrade && this.transformerEnabled == true) {
+      this.score++;
+      this.flywheel -= 2 * this.transformerUpgrade;
+      this.transformer += 2 * this.transformerUpgrade;
+      this.money += 2 * this.transformerUpgrade;
+      localStorage.setItem('flywheel', this.flywheel.toString());
+      localStorage.setItem('transformer', this.transformer.toString());
+      localStorage.setItem('money', this.money.toString());
+    }
   }
 }
+
